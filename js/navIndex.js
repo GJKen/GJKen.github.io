f1c39b1651915c74a0ef3debc9498106b79848e7
gjken <gjkenryan@gmail.com>
2022-08-02 01:46:58 +0800

    update 2022/08/02  1:46

--- js/navIndex.js
+++ js/navIndex.js
@@ -1,25 +1,24 @@
-var list = document.querySelectorAll('li');
-var color;
-for (let i = 0; i < list.length; i++){
-	list[i].onmouseenter = function(){
-		let j = 0;
-		while (j < list.length){
-			list[j++].className = '';
-		}
-		list[i].className = 'active';
-		list[i].style.borderColor = color;
-	}
-}
+var body = document.body.style;
+
+// 导航 icon 动画
+var list = document.querySelectorAll('.list');
 list.forEach(elements =>{
-	elements.addEventListener('mouseenter',function(event){
-		let bg = document.querySelector('body');
-		color = event.target.getAttribute('data-color');
-		bg.style.background = color;
+	elements.addEventListener('mouseenter',(event)=>{
+		let color = event.target.dataset.color;
+		let style = event.target.style;
+		style.borderRight = '3px solid'+color;
+		body.background = color;
+	})
+	elements.addEventListener('mouseleave',(event)=>{
+		let color = event.target.dataset.color;
+		event.target.removeAttribute('style');
+		body.removeAttribute('style');
 	})
 })
 
+// 随机文字函数
 function helloSpan(){
-	let emjoiList = ["❤️涩涩❤", "😢悲伤😢", "🖐️好冲🖐️", "😤傲慢😤", "🤠野性🤠"];
+	let emjoiList = ["❤️涩涩❤️", "😢悲伤😢", "🖐️好冲🖐️", "😤傲慢😤", "🤠野性🤠"];
 	let num = Math.floor((Math.random() * 5));
 	var sese = document.getElementById('hello_span');
 	sese.innerHTML = emjoiList[num];
